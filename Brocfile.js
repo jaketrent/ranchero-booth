9c89facaf6451c5a466deef46453d3b5130912e1
jaketrent <trent.jake@gmail.com>
2014-03-23 08:55:14 -0600

    make stache placement and multiple moves work

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,39 @@
+var concat = require('broccoli-concat')
 var jade = require('broccoli-jade')
 var pickFiles = require('broccoli-static-compiler')
+var stylus = require('broccoli-stylus')
+var traceur = require('broccoli-traceur')
 
 module.exports = function (broccoli) {
 
-  var viewsTree = broccoli.makeTree('app/views')
+  var jsTree = broccoli.makeTree('app/js/lib')
+  jsTree = traceur(jsTree)
+  jsTree = pickFiles(jsTree, {
+    srcDir: '',
+    destDir: 'lib'
+  })
 
+  jsTree = concat(jsTree, {
+    inputFiles: [
+      'lib/**/*.js'
+    ],
+    outputFile: '/js/dist.js'
+  })
+
+  var viewsTree = broccoli.makeTree('app/views')
   viewsTree = jade(viewsTree)
 
-  return viewsTree
+  var cssTree = broccoli.makeTree('app/css')
+  cssTree = pickFiles(stylus(cssTree), {
+    srcDir: '',
+    destDir: 'css'
+  })
+
+  var imgTree = broccoli.makeTree('app/img')
+  imgTree = pickFiles(imgTree, {
+    srcDir: '',
+    destDir: 'img'
+  })
+
+  return [ jsTree, viewsTree, cssTree, imgTree ]
 }
--- app/js/lib/app.js
+++ app/js/lib/app.js
@@ -1,5 +1,95 @@
-//$(=> {
-//  alert('doc ready')
-//})
+console.log('App started')
 
-console.log('app started')
+var fileInput = document.querySelector('.file')
+var canvas = document.querySelector('.canvas')
+//var download = document.querySelector('.download')
+var ctx = canvas.getContext('2d')
+var mouseIsDown = false
+
+var stachePos = { x: 0, y: 0 }
+var grabOffset = { x: 0, y: 0 }
+
+var stache = document.createElement('img')
+var avatar = document.createElement('img')
+
+document.addEventListener('DOMContentLoaded', () => {
+  fileInput.addEventListener('change', (evt) => {
+
+    var file = evt.target.files[0]
+
+    // validate file type
+    if (!file.type.match(/image.*/)) {
+      alert('Select an image file'); return
+    }
+
+    // setup preview img
+    avatar.file = file
+
+    // draw img on canvas
+    avatar.onload = () => {
+      ctx.drawImage(avatar, 0, 0, canvas.width, avatar.height * (canvas.height / avatar.width))
+
+      stache.src = 'img/stache-swirl-sm.png'
+      stache.onload = () => {
+        ctx.drawImage(stache, 0, 0, stache.width, stache.height)
+      }
+    }
+
+    // read file
+    var reader = new FileReader()
+    reader.onload = ((aImg) => { return (e) => { aImg.src = e.target.result } })(avatar)
+    reader.readAsDataURL(file)
+
+  })
+
+  var isHit = (x, y) => {
+    var withinX = stachePos.x <= x && x <= (stachePos.x + stache.width)
+    var withinY = stachePos.y <= y && y <= (stachePos.y + stache.height)
+
+    return withinX && withinY
+  }
+
+  var saveGrabOffset = (x, y) => {
+    grabOffset = { x: x, y: y }
+  }
+
+  var saveStachePos = (x, y) => {
+    stachePos = { x: x, y: y }
+  }
+
+  var redrawStache = () => {
+    ctx.drawImage(avatar, 0, 0, canvas.width, avatar.height * (canvas.height / avatar.width))
+    ctx.drawImage(stache, stachePos.x, stachePos.y, stache.width, stache.height)
+  }
+
+  var getCoords = (e) => {
+    var rect = canvas.getBoundingClientRect()
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+    return [x, y]
+  }
+
+  canvas.addEventListener('mousedown', (e) => {
+    var [x, y] = getCoords(e)
+    if (isHit(x, y)) {
+      saveGrabOffset(x - stachePos.x, y - stachePos.y)
+      mouseIsDown = true
+    }
+  })
+
+  canvas.addEventListener('mousemove', (e) => {
+    if (mouseIsDown) {
+      var [x, y] = getCoords(e)
+      saveStachePos(x - grabOffset.x, y - grabOffset.y)
+      redrawStache()
+    }
+  })
+
+  canvas.addEventListener('mouseup', (e) => {
+    mouseIsDown = false
+  })
+
+//  download.addEventListener('click', (e) => {
+//    this.href = canvas.toDataURL('image/jpeg')
+//  })
+})
